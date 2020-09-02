c91e51a194420cb0eff634673c84018e3dd35b20
frdbonif <65509453+frdbonif@users.noreply.github.com>
2020-09-02 10:53:23 +0100

    Organised menu's.

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+// Station link lookup
+
+var xmlhttp = new XMLHttpRequest();
+XMLHttpRequest.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var linksObj = JSON.parse(this.responseText);
+  }
+};
+
+xmlhttp.open("GET", "/js/links.json", true);
+xmlhttp.send();
+
+function stnLink(crs) {
+  console.log(`Looking up Arr/Dep link for ${crs}`);
+}
+
+function mapLink(crs) {
+  console.log(`Looking up map link for ${crs}`)
+}
