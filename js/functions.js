247cdc03ac6a214b2c0577a1056db187043502d0
frdbonif <65509453+frdbonif@users.noreply.github.com>
2020-09-04 23:53:37 +0100

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -4,13 +4,13 @@ var xmlhttp = new XMLHttpRequest();
 XMLHttpRequest.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var linksObj = JSON.parse(this.responseText);
+    console.log(linksObj)
   }
 };
 
 xmlhttp.open("GET", "/js/links.json", true);
 xmlhttp.send();
 
-console.log(linksObj)
 
 function stnLink(crs) {
   console.log(`Looking up Arr/Dep link for ${crs}`);
